d10147962e32b63c76daa5adfe5e2c5a168e0807
mduckdev <mduckdev@gmail.com>
2022-11-09 15:44:41 +0100

    Equation width fix and added python plots

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask import request, send_from_directory
+import subprocess
+import os
+
+app = Flask(__name__, static_folder='./lagrange/build')
+
+
+@app.route("/showPlot", methods=["POST"])
+def post():
+    if(request.json):
+        x_points = request.json['xPoints']
+        y_points = request.json['yPoints']
+        params = ["python", ".\\plot.py"]
+        for i in range(len(x_points)):
+            params.append(str(x_points[i])+","+str(y_points[i]))
+
+        subprocess.Popen(params)
+        return request.json
+    return "Hello"
+
+
+@app.route("/ping")
+def get():
+    return "Online"
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, port=5000, threaded=True)
---
+++ plot.py
@@ -0,0 +1,53 @@
+from mpl_toolkits.axisartist.axislines import AxesZero
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+x_points = []
+y_points = []
+
+for index, i in enumerate(sys.argv):
+    if(index == 0):
+        continue
+    pair = i.split(",")
+    x_points.append(float(pair[0]))
+    y_points.append(float(pair[1]))
+
+print(x_points, y_points)
+
+
+def interpolate_x_value(X):
+    yp = 0
+    for i in range(len(x_points)):
+        p = 1
+        for j in range(len(x_points)):
+            if i != j:
+                p = p * (X - x_points[j])/(x_points[i] - x_points[j])
+        yp = yp + p * y_points[i]
+    return yp
+
+
+plt.rcParams["figure.figsize"] = [100, 100]
+plt.rcParams["figure.autolayout"] = True
+fig = plt.figure()
+ax = fig.add_subplot(axes_class=AxesZero)
+#ax.set_aspect('equal', adjustable='box')
+figManager = plt.get_current_fig_manager()
+figManager.window.showMaximized()
+
+for direction in ["xzero", "yzero"]:
+    ax.axis[direction].set_axisline_style("-|>")
+
+    ax.axis[direction].set_visible(True)
+
+for direction in ["left", "right", "bottom", "top"]:
+    ax.axis[direction].set_visible(False)
+
+plt.plot(x_points, y_points, 'r*')
+
+x = np.linspace(min(x_points)-len(x_points)*2,
+                max(x_points)+len(x_points)*2, 200)
+
+ax.plot(x, interpolate_x_value(x))
+
+
+plt.show()
